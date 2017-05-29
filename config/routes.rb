2932a7c1cb258eef4f733469a6cd5425e30bc746
Constantin EhrlichAdam <c.ehrlichadam@hotmail.com>
2017-05-29 15:05:09 +0100

    Edit profile page for user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'users#index'
 
+  # Edit my profile page
+  resources :users, only: [:edit, :update]
+
+  # CRUD for events
   resources :events, except: [:index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
