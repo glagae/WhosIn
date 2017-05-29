e8c08078d4a7589680fcf39f7f993d65b3c98266
Constantin EhrlichAdam <c.ehrlichadam@hotmail.com>
2017-05-29 14:49:13 +0100

    Validations for the model

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,4 +1,10 @@
 class MenuItem < ApplicationRecord
+  # Associtations
   belongs_to :invitation
   belongs_to :event
+
+  # Validations
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :bring, presence: true
 end
