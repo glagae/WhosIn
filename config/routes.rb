1696b8ef479ab092dd224f432e059febd5ce2687
Gauthier Lagae <gauthier.lagae@gmail.com>
2017-05-30 12:19:57 +0100

    Facebook connect available and edit profile working bitches

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  root to: 'events#index'
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  root to: 'pages#home'
 
   # Edit my profile page
   resources :users, only: [:edit, :update]
