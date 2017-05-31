aaf75494cd2f465ee8ca129aad403e882141269c
Gauthier Lagae <gauthier.lagae@gmail.com>
2017-05-31 16:32:28 +0100

    heroku friend list

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -5,7 +5,10 @@ class FriendsController < ApplicationController
 
   def fb_friends
     # access_token and other values aren't required if you set the defaults as described above
-  @current_user = current_user
-  @fb_friends = @current_user.facebook.get_connection("me","friends")
+    db_friends = []
+
+    db_friends = current_user.facebook.get_connection("me","friends").map do |friend|
+    friend.name
+    end
   end
 end
