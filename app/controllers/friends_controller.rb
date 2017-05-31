74f12336c2990269b246d9d7131bf5c55ac4c5e1
Constantin EhrlichAdam <c.ehrlichadam@hotmail.com>
2017-05-31 16:34:15 +0100

    Menu display

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -5,7 +5,6 @@ class FriendsController < ApplicationController
 
   def fb_friends
     # access_token and other values aren't required if you set the defaults as described above
-  @current_user = current_user
-  @fb_friends = @current_user.facebook.get_connection("me","friends")
+    @fb_friends = current_user.facebook.get_connection("me","friends")
   end
 end
